1254654878d80af1326c2b07d06073287595b357
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2023-03-02 14:04:36 -0300

    z006 get_string

--- src/z006_creando_variables_nuevas_usando_rpart_rules.r
+++ src/z006_creando_variables_nuevas_usando_rpart_rules.r
@@ -24,9 +24,12 @@ df <- fread("./data/ibarra_generacion.txt.gz")
 
 df[, clase01 :=  ifelse( clase=="SI", 1, 0 )]
 
-# Rpart no tan grande
+#=======================================================
+# VARIABLES BOOLEANAS DERIVADAS DE RPART.RULES
 
-max.depth <- 6
+#Con un árbol pequeño. 
+
+max.depth <- 10
 num.leaves.teorico <- max.depth**2
 kfolds=5
 reg = ((kfolds-1)/kfolds)*nrow(df)
@@ -49,11 +52,35 @@ nrules <- nrow(rules)
 
 
 get_string = function(x){
-  s = str_replace_all(str_squish(paste(rules[x, 3:73], collapse = " ")), " is "," == ")
+  s = str_squish(paste(rules[x, 3:73], collapse = " "))
+  # cat(s,"(1)\n")
+  splitted = unlist(str_split(s, pattern = " "))
+  to_ids = which("to" == splitted)
+  # cat(to_ids, "to_ids\n")
+  for (to_id in to_ids){
+    value1 = splitted[to_id - 1]
+    value2 = splitted[to_id + 1]
+    to = splitted[to_id]
+    is = splitted[to_id - 2]
+    variable = splitted[to_id - 3]
+    sub_to_reeplace = paste(variable, is, value1, to, value2, collapse = " ")
+    # cat(sub_to_reeplace, "\n")
+    new_sub = paste(variable,">", value1, "&", variable, "<", value2, collapse = " ")
+    s = str_replace(s, sub_to_reeplace, new_sub)
+    # cat(s,"(2)\n")
+  }
+  s = str_replace_all(s, " is "," == ")
+  # cat(s,"(3)\n")
   return(paste0("as.integer(",s,")"))
 }
   
-
+#Para cada regla creadas armo una variable booleana. 
 j <- seq_len( nrules )
 df[ , paste0("campo", j) := lapply( j, function(x) eval(parse(text = get_string(x)))) ]
+
+
+#==========================================================
+
+
+
 
